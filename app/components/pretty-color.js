cce185c1e70188c88757869b3740e3666be33be6
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-19 07:06:49 -0500

    Fixes errors from testing docs

--- app/components/pretty-color.js
+++
@@ -1,8 +0,0 @@
-export default Ember.Component.extend({
-  attributeBindings: ['style'],
-
-  style: Ember.computed('name', function() {
-    const name = this.get('name');
-    return `color: ${name}`;
-  })
-});
--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
 import Application from '../../app';
-import Router from '../../router';
 import config from '../../config/environment';
-import './should-have-element-with-count';
-import './dblclick';
-import './add-contact';
 
 export default function startApp(attrs) {
   let application;
