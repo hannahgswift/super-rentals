340751bedaf72435e57f62bea055c24c75689d3f
Hannah Swift <h.graceswift@gmail.com>
2016-07-18 22:28:51 -0500

    Errors, nothing displaying on localhost:4200

---
+++ tests/helpers/should-have-element-with-count.js
@@ -0,0 +1,9 @@
+export default Ember.Test.registerHelper('shouldHaveElementWithCount',
+  function(app, assert, selector, n, context) {
+    const el = findWithAssert(selector, context);
+    const count = el.length;
+    assert.equal(n, count, `found ${count} times`);
+  }
+);
+
+// shouldHaveElementWithCount(assert, 'ul li', 3);
